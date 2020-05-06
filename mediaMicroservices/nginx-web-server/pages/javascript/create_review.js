972a944283545a5d93fdb8a0b826efd169b6e2f7
zixiao1511034 <zixiao1511034@outlook.com>
2020-05-06 17:21:46 -0400

    create review

--- mediaMicroservices/nginx-web-server/pages/javascript/create_review.js
+++ mediaMicroservices/nginx-web-server/pages/javascript/create_review.js
@@ -1,13 +1,38 @@
+function uploadPost() {
+  if (document.getElementById("post-content").value !== "") {
+    const Http = new XMLHttpRequest();
+    const url =
+      "http://" + window.location.hostname + ":18080/wk2-api/review/compose";
+    Http.open("POST", url, true);
+    var body =
+      "text=" +
+      document.getElementById("post-content").value +
+      "&username=username_1&password=password&rating=" +
+      userRating +
+      "&title=" +
+      document.getElementById("movie-title").value;
+    alert(body);
+    Http.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(Http.responseText);
+      }
+    };
+    Http.send(body);
+    window.location.reload();
+  }
+}
+
 var hide = document.getElementById("hide-post");
 var show = document.getElementById("show-post");
+var userRating;
 
 hide.addEventListener("click", function () {
   $("#compose").hide();
 });
 
 // show.addEventListener("click", function () {
-//     $("#compose").show();
-// })
+//   $("#compose").show();
+// });
 show.addEventListener("click", function () {
   $("#compose").toggle();
 });
@@ -22,7 +47,6 @@ $(document).ready(function () {
   });
 
   $("input:radio").change(function () {
-    var userRating = this.value;
-    alert(userRating);
+    userRating = this.value;
   });
 });
