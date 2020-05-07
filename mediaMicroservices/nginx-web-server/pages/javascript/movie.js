906b619758cec12a58ddcd548d3fb376d2ed05ce
Zhongkai Liu <zl777@cornell.edu>
2020-05-06 22:58:15 -0400

    make upload review

--- mediaMicroservices/nginx-web-server/pages/javascript/movie.js
+++ mediaMicroservices/nginx-web-server/pages/javascript/movie.js
@@ -1,3 +1,19 @@
+const generateStars = (num) => {
+    const star = '<span class="rating-star">&#9733</span>';
+    const star_active = '<span class="rating-star star-active">&#9733</span>';
+
+    let res = "";
+    for (let i = 1; i <= 5; i++) {
+        if (i <= num) {
+            res += star_active;
+        } else {
+            res += star;
+        }
+    }
+
+    return res;
+};
+
 // get movie id from URL
 const getMovieId = () => {
     let vars = [];
@@ -15,7 +31,8 @@ const getMovieId = () => {
 // send movie id to Lua
 // return JSON file
 const sendMovieId = (movieId) => {
-    let url = ""; // TODO: get URL there
+    // const url = "http://" + window.location.hostname + ":18080/wk2-api/movie-info/read";
+    const url = "/wk2-api/movie-info/read";
     let sendData = { movie_id: movieId };
     let returnData;
     $.getJSON(url, sendData, function (data) {
@@ -27,9 +44,11 @@ const sendMovieId = (movieId) => {
 // generate review list
 const generateReviewList = (reviewList) => {
     reviewList.forEach(function (item, i) {
+        let rating = item["rating"];
+        let stars = generateStars(rating);
         let reviewItem =
             '<div class="card review-list-item"><div class="card-header"><p id="review-list-username">Anonymous User</p><p id="review-list-rating">Rating:' +
-            item["rating"] +
+            stars +
             '</p></div><div class="card-body"><p class="card-text">' +
             item["text"] +
             "</p></div></div>";
@@ -52,16 +71,18 @@ const renderData = (data) => {
     console.log(data);
     // get data
     let title = data["title"];
-    let rating = data["rating"];
     let plot = data["plot"];
+    let rating = Number(data["movie_id"]["avg_rating"]);
+    rating = Math.floor(rating);
     // check the following index
     let reviews = data["reviews"];
     let casts = data["cast_infos"];
 
     // fill data in HTML
     $("#data-title").text(title);
-    $("#data-rating").text(rating);
     $("#data-plot").text(plot);
+
+    $("#data-rating").html(generateStars(rating));
 
     generateReviewList(reviews);
     generateCastList(casts);
@@ -69,8 +90,8 @@ const renderData = (data) => {
 
 // main codes
 
-let movieId = getMovieId();
+// let movieId = getMovieId();
 
-let data = sendMovieId(movieId);
+// let data = sendMovieId(movieId);
 
-renderData(data);
+// renderData(data);
---
+++ mediaMicroservices/nginx-web-server/pages/javascript/upload_review.js
@@ -0,0 +1,63 @@
+const uploadReview = (uploadRating) => {
+    // const url = "http://" + window.location.hostname + ":18080/wk2-api/review/compose";
+    const url = "/wk2-api/review/compose";
+    const username = "username_1";
+    const password = "password_1";
+    let title = $("#data-title").text().trim();
+    let review = $("#upload-text").val().trim();
+    let rating = uploadRating;
+    const data = {
+        title: title,
+        text: review,
+        username: username,
+        password: password,
+        rating: rating,
+    };
+    console.log(data);
+    if (rating > 0 && review != "" && review != null) {
+        $.ajax(url, {
+            data: data,
+            type: "POST",
+            contentType: "application/x-www-form-urlencoded",
+            success: function (data, textStatus, jqXHR) {
+                console.log("success upload review");
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("error:" + errorThrown);
+            },
+        });
+    }
+};
+
+// upload stars
+
+let stars = $(".upload-star");
+let starsDiv = $("#upload-rating-div");
+var uploadRating = 0;
+
+stars.on("click", function (event) {
+    let index = stars.index($(this)) + 1;
+    for (let i = 1; i <= 5; i++) {
+        if (i <= index) {
+            $("#star" + i).addClass("star-active");
+        } else {
+            $("#star" + i).removeClass("star-active");
+        }
+    }
+
+    uploadRating = index;
+});
+
+// buttons
+$("#review-button").click(function () {
+    $("#upload-div").toggleClass("hidden");
+});
+
+$("#upload-button").click(function () {
+    console.log("hello");
+    uploadReview(uploadRating);
+});
+
+$("#cancel-button").click(function () {
+    $("#upload-div").addClass("hidden");
+});
